876a66507c9fd927127915ea428e91866eaf416a
ingrid fleury <ingridfleury1@gmail.com>
2021-06-30 23:08:16 +0100

    Creation of the Worker class

---
+++ ObjectComposition/Worker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectComposition
+{
+    class Worker
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+
+    }
+}
