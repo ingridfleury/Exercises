70e1ad4216d29f848fac0780e617bf7b3db557f1
ingrid fleury <ingridfleury1@gmail.com>
2021-06-30 23:30:57 +0100

    Creation of the HourContract and Departament class

---
+++ ObjectComposition/Entities/Departament.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectComposition.Entities
+{
+    class Departament
+    {
+        public int Id { get; set; }
+        public string  Name { get; set; 
+
+    }
+}
---
+++ ObjectComposition/Entities/HourContract.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectComposition.Entities
+{
+    class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+    }
+
+}
