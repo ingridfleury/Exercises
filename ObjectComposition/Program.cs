53accde11be0548f1de8d817b73c9a62e3a4a261
ingrid fleury <ingridfleury1@gmail.com>
2021-07-01 22:54:52 +0100

    Main program creation

--- ObjectComposition/Program.cs
+++ ObjectComposition/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using ObjectComposition.Entities.Enums;
 using ObjectComposition.Entities;
-using System.Globalization
+using System.Globalization;
+
 namespace ObjectComposition
 {
     class Program
@@ -21,6 +22,32 @@ namespace ObjectComposition
             Departament dept = new Departament(deptName);
             Worker worker = new Worker(name, level, baseSalary, dept);
 
+            Console.WriteLine("How many contracts to this worker?");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} contract data: ");
+                Console.Write("Date (DD/MM/YYYY): ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine("Duration (hours): ");
+                int hours = int.Parse(Console.ReadLine());
+                HourContract contract = new HourContract(date, valuePerHour, hours);
+                worker.AddContract(contract);
+
+            }
+
+            Console.WriteLine();
+            Console.Write("Enter month and year to calculate income (MM/YYYY): ");
+            string monthAndYear = Console.ReadLine();
+            int month = int.Parse(monthAndYear.Substring(0, 2));
+            int year = int.Parse(monthAndYear.Substring(3));
+            Console.WriteLine("Name: " + worker.Name);
+            Console.WriteLine("Department" + worker.Departament.Name);
+            Console.WriteLine("Income for " + monthAndYear + ": " + worker.Income(year, month));
+             
 
         }
     }
